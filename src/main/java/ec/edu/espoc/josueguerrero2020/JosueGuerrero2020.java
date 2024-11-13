c6cf9c7587667985fe30b405add98b920406e261
OswaldoBifarini <jgbifarini@gmail.com>
2024-11-13 13:43:12 -0500

    Segundo comentario

--- src/main/java/ec/edu/espoc/josueguerrero2020/JosueGuerrero2020.java
+++ src/main/java/ec/edu/espoc/josueguerrero2020/JosueGuerrero2020.java
@@ -4,13 +4,25 @@
 
 package ec.edu.espoc.josueguerrero2020;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DELL
  */
+   
 public class JosueGuerrero2020 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //1. daros
+        int [] puntuacionesDiarias = int[7];
+        int promedioPuntuaciones=0;
+        boolean puntuacionAltaBaja= false;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        //ingreso de puntuaciones
+        for (int i=0; i<puntuacionesDiarias.length;i++){
+            System.out.println("Ingrese la puntualcion de cada dia");}
     }
 }
