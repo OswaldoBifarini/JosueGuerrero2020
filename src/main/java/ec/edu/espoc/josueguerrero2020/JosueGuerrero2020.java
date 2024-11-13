1a93c72edc8bb8c9e02bc16df1b9c7ddca3a4782
OswaldoBifarini <jgbifarini@gmail.com>
2024-11-13 14:43:00 -0500

    Tercer comentario

--- src/main/java/ec/edu/espoc/josueguerrero2020/JosueGuerrero2020.java
+++ src/main/java/ec/edu/espoc/josueguerrero2020/JosueGuerrero2020.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ec.edu.espoc.josueguerrero2020;
 
 import java.util.Scanner;
@@ -10,19 +9,53 @@ import java.util.Scanner;
  *
  * @author DELL
  */
-   
 public class JosueGuerrero2020 {
 
     public static void main(String[] args) {
         //1. daros
-        int [] puntuacionesDiarias = int[7];
-        int promedioPuntuaciones=0;
-        boolean puntuacionAltaBaja= false;
+        int[] puntuacionesDiarias = new int[7];
+        double promedioPuntuaciones = 0;
+        double puntuacionAlta = 0;
+        Scanner sc = new Scanner(System.in);
+        int dato = 0;
+        int i = 0;
+        do {
+            System.out.println("Ingresar los valores de las puntuaciones");
+            dato = sc.nextInt();
+            if (dato > 0) {
+                puntuacionesDiarias[i] = dato;
+                i++;
+            } else {
+                System.out.println("El Valor es negativo");
+            }
+
+        } while (i < 7);
+        int opcion;
+        do {
+            System.out.println("Acciones a realizar");
+            System.out.println("1. Calcular el total de puntuaciones de la semana");
+            System.out.println("2. Calcular el promedio de puntuaciones y mostrar los dias por encima y por debajo del promedio");
+            System.out.println("3. Mostrar el dia con la puntuacion mas alta y el dia con la mas Baja");
+            System.out.println("4. Calcular el total de puntuaciones acumuladas durante los primeros cuatro dias u los ultimos cuatro dias ");
+            System.out.println("5. Salir");
+            
+            switch (opcion);
+                    case 1://1. . Calcular el total de puntuaciones de la semana
+                        for(int )
+        }while(opcion !=4);
         
-        Scanner scanner = new Scanner(System.in);
-        
-        //ingreso de puntuaciones
-        for (int i=0; i<puntuacionesDiarias.length;i++){
-            System.out.println("Ingrese la puntualcion de cada dia");}
     }
 }
+
+//ingreso de puntuaciones
+        
+        
+        /*if(puntuacionesDiarias<0) {System.out.println("Valor incorrecto ingrese un valor positivo");
+            
+        } else {for (int i=0; i<puntuacionesDiarias.length;i++){
+            System.out.println("Ingrese la puntualcion de cada dia"+puntuacionesDiarias);}
+        }
+        
+    }
+ }
+*/
