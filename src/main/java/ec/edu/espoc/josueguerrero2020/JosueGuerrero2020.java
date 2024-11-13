f96b581f9dbaf5bce2dc0120c31f4fbe2e527f3d
OswaldoBifarini <jgbifarini@gmail.com>
2024-11-13 13:40:39 -0500

    Primer comentario

---
+++ src/main/java/ec/edu/espoc/josueguerrero2020/JosueGuerrero2020.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoc.josueguerrero2020;
+
+/**
+ *
+ * @author DELL
+ */
+public class JosueGuerrero2020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
